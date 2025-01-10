80a90cfcef940ec22598e2f5634c019eae404179
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-01-11 01:08:07 +0700

    updated

---
+++ lesson00-interview/src/less04_ex04_inheritance/Manager.java
@@ -0,0 +1,33 @@
+package less04_ex04_inheritance;
+
+// composition
+public class Manager {
+	
+	private BaseEmployee baseEmployee;
+	private Integer levelUpTitle;
+	private String department;
+	
+	public Manager() {
+	}
+
+	public Manager(BaseEmployee baseEmployee, Integer levelUpTitle, String department) {
+		this.baseEmployee = baseEmployee;
+		this.levelUpTitle = levelUpTitle;
+		this.department = department;
+	}
+	
+	// Class 1: A B C D E F G --> Base 1, Base 2
+	
+	// Class 2: A B C E F --> Base 1
+	
+	// Class 3: D G  --> Base 2
+	
+	// Class 4: A B C D E F --> Base 1, D
+	
+	// Class 5: D G  --> Base 2
+	
+	// Base 1: A B C E F
+	// Base 2: D F G
+	
+	
+}
