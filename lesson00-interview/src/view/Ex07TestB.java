6b6f32b275fade3c72caddc410fe4cdab89c032b
Huy Doan <doanvanhuydhdtu@gmail.com>
2025-01-10 21:19:38 +0700

    updated

---
+++ lesson00-interview/src/view/Ex07TestB.java
@@ -0,0 +1,20 @@
+package view;
+
+public class Ex07TestB /*extends Ex07TestC*/{
+	
+	public void method01() {
+		
+	}
+	
+	public final void method02() {
+		
+	}
+	
+	private void method03() {
+		
+	}
+	
+	public static void method04() {
+		
+	}
+}
---
+++ lesson00-interview/src/view/Ex07TestC.java
@@ -0,0 +1,5 @@
+package view;
+
+public final class Ex07TestC {
+
+}
